848b3f710daf5ca0d488c86ca17cc62b3cc399b9
James <118912301+jclar0@users.noreply.github.com>
2022-12-27 17:44:41 +0000

    Initial commit

---
+++ Launcher_Home.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Cosmic
+{
+    partial class Launcher_Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playGame = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // playGame
+            // 
+            this.playGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.playGame.BackColor = System.Drawing.Color.White;
+            this.playGame.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(78)))), ((int)(((byte)(54)))));
+            this.playGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playGame.ForeColor = System.Drawing.Color.Black;
+            this.playGame.Location = new System.Drawing.Point(13, 480);
+            this.playGame.Name = "playGame";
+            this.playGame.Size = new System.Drawing.Size(135, 41);
+            this.playGame.TabIndex = 0;
+            this.playGame.Text = "Play Game";
+            this.playGame.UseVisualStyleBackColor = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(13, 13);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(689, 461);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // Launcher_Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(714, 533);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.playGame);
+            this.Name = "Launcher_Home";
+            this.Text = "Cosmic Launcher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playGame;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
